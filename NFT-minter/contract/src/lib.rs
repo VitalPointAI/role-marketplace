92e5a8c306951ba55153ed156e3e754ab2adfc0b
Braeden0415 <slashslayer19@hotmail.com>
2021-11-20 16:59:44 -0500

    Initial contract for NFT minting.

--- NFT-minter/contract/src/lib.rs
+++ NFT-minter/contract/src/lib.rs
@@ -1,121 +1,99 @@
-/*
- * This is an example of a Rust smart contract with two simple, symmetric functions:
- *
- * 1. set_greeting: accepts a greeting, such as "howdy", and records it for the user (account_id)
- *    who sent the request
- * 2. get_greeting: accepts an account_id and returns the greeting saved for it, defaulting to
- *    "Hello"
- *
- * Learn more about writing NEAR smart contracts with Rust:
- * https://github.com/near/near-sdk-rs
- *
- */
-
-// To conserve gas, efficient serialization is achieved through Borsh (http://borsh.io/)
+use near_contract_standards::non_fungible_token::metadata::{
+    NFTContractMetadata, NonFungibleTokenMetadataProvider, TokenMetadata, NFT_METADATA_SPEC,
+};
+use near_sdk::{ env, near_bindgen, setup_alloc};
+use near_contract_standards::non_fungible_token::{Token, TokenId};
+use near_contract_standards::non_fungible_token::NonFungibleToken;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{env, near_bindgen, setup_alloc};
-use near_sdk::collections::LookupMap;
+use near_sdk::collections::LazyOption;
+use near_sdk::json_types::ValidAccountId;
+use near_sdk::{ AccountId, BorshStorageKey, PanicOnDefault, Promise, PromiseOrValue,
+};
 
-setup_alloc!();
+near_sdk::setup_alloc!();
 
-// Structs in Rust are similar to other languages, and may include impl keyword as shown below
-// Note: the names of the structs are not important when calling the smart contract, but the function names are
+//Storage
 #[near_bindgen]
-#[derive(BorshDeserialize, BorshSerialize)]
-pub struct Welcome {
-    records: LookupMap<String, String>,
+#[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
+pub struct Contract {
+    tokens: NonFungibleToken,
+    metadata: LazyOption<NFTContractMetadata>,
 }
 
-impl Default for Welcome {
-  fn default() -> Self {
-    Self {
-      records: LookupMap::new(b"a".to_vec()),
+// Minting funtion 
+#[near_bindgen]
+impl Contract{
+    #[payable]
+     pub fn nft_mint(
+     &mut self,
+     token_id: TokenId,
+     receiver_id: ValidAccountId,
+     token_metadata: TokenMetadata,
+) -> Token {
+    self.tokens.mint(token_id, receiver_id, Some(token_metadata))
     }
-  }
 }
 
+//Declaring the different subtypes within the StorageKey type.
+#[derive(BorshSerialize, BorshStorageKey)]
+enum StorageKey {
+    Metadata,
+    NonFungibleToken,
+    Enumeration,
+    TokenMetadata,
+    Approval,
+}
+
+// image/icon for default metadata
+const TEST_IMAGE: &str = "https://bafkreic5tytkkxr4zojgakkafn7mdtoptct7yzrkrocnrbgvgmi3z4gdu4.ipfs.dweb.link/";
+
+
+
+
+
 #[near_bindgen]
-impl Welcome {
-    pub fn set_greeting(&mut self, message: String) {
-        let account_id = env::signer_account_id();
-
-        // Use env::log to record logs permanently to the blockchain!
-        env::log(format!("Saving greeting '{}' for account '{}'", message, account_id,).as_bytes());
-
-        self.records.insert(&account_id, &message);
+impl Contract {
+    /// Initializes the contract owned by `owner_id` with
+    /// default metadata
+    #[init]
+    pub fn new_default_meta(owner_id: ValidAccountId) -> Self {  //declaring new function which intakes an owner id and creates default metadata for the account within the pub struct 'Contract'
+        Self::new(
+            owner_id,
+            NFTContractMetadata {  //specifies what the default metadata is set to.
+                spec: NFT_METADATA_SPEC.to_string(),
+                name: "New NEAR non-fungible token test".to_string(),
+                symbol: "EXAMPLE".to_string(),
+                icon: Some(TEST_IMAGE.to_string()),
+                base_uri: None,
+                reference: None,
+                reference_hash: None,
+            },
+        )
     }
 
-    // `match` is similar to `switch` in other languages; here we use it to default to "Hello" if
-    // self.records.get(&account_id) is not yet defined.
-    // Learn more: https://doc.rust-lang.org/book/ch06-02-match.html#matching-with-optiont
-    pub fn get_greeting(&self, account_id: String) -> String {
-        match self.records.get(&account_id) {
-            Some(greeting) => greeting,
-            None => "Hello".to_string(),
+    #[init]
+    pub fn new(owner_id: ValidAccountId, metadata: NFTContractMetadata) -> Self {
+        assert!(!env::state_exists(), "Already initialized"); //checks to see if default metadata has already been created. ie the contract has been initialized already.
+        metadata.assert_valid();
+        Self {  //self refers to Contract struct
+            tokens: NonFungibleToken::new(  //calling the 'new' method to be defined by the NonFungibleToken type to log and save the new tokens data.
+                StorageKey::NonFungibleToken,
+                owner_id,
+                Some(StorageKey::TokenMetadata),
+                Some(StorageKey::Enumeration),
+                Some(StorageKey::Approval),
+            ),
+            metadata: LazyOption::new(StorageKey::Metadata, Some(&metadata)), //LazyOption is a struct from the nearsdk collection, this struct function is to store the value for a given key. In this case it is the NFTs metadata.
         }
     }
 }
 
-/*
- * The rest of this file holds the inline tests for the code above
- * Learn more about Rust tests: https://doc.rust-lang.org/book/ch11-01-writing-tests.html
- *
- * To run from contract directory:
- * cargo test -- --nocapture
- *
- * From project root, to run in combination with frontend tests:
- * yarn test
- *
- */
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use near_sdk::MockedBlockchain;
-    use near_sdk::{testing_env, VMContext};
-
-    // mock the context for testing, notice "signer_account_id" that was accessed above from env::
-    fn get_context(input: Vec<u8>, is_view: bool) -> VMContext {
-        VMContext {
-            current_account_id: "alice_near".to_string(),
-            signer_account_id: "bob_near".to_string(),
-            signer_account_pk: vec![0, 1, 2],
-            predecessor_account_id: "carol_near".to_string(),
-            input,
-            block_index: 0,
-            block_timestamp: 0,
-            account_balance: 0,
-            account_locked_balance: 0,
-            storage_usage: 0,
-            attached_deposit: 0,
-            prepaid_gas: 10u64.pow(18),
-            random_seed: vec![0, 1, 2],
-            is_view,
-            output_data_receivers: vec![],
-            epoch_height: 19,
-        }
-    }
-
-    #[test]
-    fn set_then_get_greeting() {
-        let context = get_context(vec![], false);
-        testing_env!(context);
-        let mut contract = Welcome::default();
-        contract.set_greeting("howdy".to_string());
-        assert_eq!(
-            "howdy".to_string(),
-            contract.get_greeting("bob_near".to_string())
-        );
-    }
-
-    #[test]
-    fn get_default_greeting() {
-        let context = get_context(vec![], true);
-        testing_env!(context);
-        let contract = Welcome::default();
-        // this test did not call set_greeting so should return the default "Hello" greeting
-        assert_eq!(
-            "Hello".to_string(),
-            contract.get_greeting("francis.near".to_string())
-        );
+near_contract_standards::impl_non_fungible_token_core!(Contract, tokens);
+near_contract_standards::impl_non_fungible_token_approval!(Contract, tokens);
+near_contract_standards::impl_non_fungible_token_enumeration!(Contract, tokens);
+#[near_bindgen]  //NFT Metadata provider/reader
+impl NonFungibleTokenMetadataProvider for Contract {
+    fn nft_metadata(&self) -> NFTContractMetadata {
+        self.metadata.get().unwrap()  //Calls for the metadata of the contract and returns their values, will panic if there is not one.
     }
 }
