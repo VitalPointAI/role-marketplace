d3ded1f5332a93bb596264fe6adc5781dc3cb489
Braeden0415 <92407622+Braeden0415@users.noreply.github.com>
2021-11-30 22:14:39 -0500

    Final Version

--- NFT-minter/contract/src/lib.rs
+++ NFT-minter/contract/src/lib.rs
@@ -17,16 +17,31 @@ near_contract_standards::impl_non_fungible_token_enumeration!(Contract, tokens);
 
 //Storage
 #[near_bindgen]
-#[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)] // Functions to pack, store, unpack data and a panic on default funtion as a saftey net in the event of no data.
+#[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)] 
 pub struct Contract {
     tokens: NonFungibleToken,
     metadata: LazyOption<NFTContractMetadata>,
 }
 
-// Minting funtion 
+
 #[near_bindgen]
 impl Contract{
-
+    
+    #[init]
+     pub fn new(owner_id: ValidAccountId, metadata: NFTContractMetadata) -> Self {
+        assert!(!env::state_exists(), "Already initialized");
+        metadata.assert_valid();
+        Self {  //self refers to Contract struct
+            tokens: NonFungibleToken::new( 
+                StorageKey::NonFungibleToken,
+                owner_id,
+                Some(StorageKey::TokenMetadata),
+                Some(StorageKey::Enumeration),
+                Some(StorageKey::Approval),
+            ),
+            metadata: LazyOption::new(StorageKey::Metadata, Some(&metadata)), 
+        } 
+    }
     
     #[payable]
      pub fn nft_mint(
@@ -37,25 +52,8 @@ impl Contract{
 ) -> Token {
     self.tokens.mint(token_id, receiver_id, Some(token_metadata))
     }
-
-    #[init]
-    pub fn new(owner_id: ValidAccountId, metadata: NFTContractMetadata) -> Self {
-        assert!(!env::state_exists(), "Already initialized"); //checks to see if default metadata has already been created. ie the contract has been initialized already.
-        metadata.assert_valid();
-        Self {  //self refers to Contract struct
-            tokens: NonFungibleToken::new(  //calling the 'new' method to be defined by the NonFungibleToken type to log and save the new tokens data.
-                StorageKey::NonFungibleToken,
-                owner_id,
-                Some(StorageKey::TokenMetadata),
-                Some(StorageKey::Enumeration),
-                Some(StorageKey::Approval),
-            ),
-            metadata: LazyOption::new(StorageKey::Metadata, Some(&metadata)), 
-        } //LazyOption is a struct from the nearsdk collection, this struct function is to store the value for a given key. In this case it is the NFTs metadata.
-    }
 }
 
-//Declaring the different subtypes within the StorageKey type.
 #[derive(BorshSerialize, BorshStorageKey)]
 enum StorageKey {
     Metadata,
@@ -71,5 +69,3 @@ impl NonFungibleTokenMetadataProvider for Contract {
         self.metadata.get().unwrap()  
     }  
 }
-
-//near call $ID nft_mint '{"token_id": "0", "receiver_id": "'$ID'", "token_metadata": { "title": "Some Art", "description": "My NFT media", "media": "https://bafkreiabag3ztnhe5pg7js4bj6sxuvkz3sdf76cjvcuqjoidvnfjz7vwrq.ipfs.dweb.link/", "copies": 1}}' --accountId $ID --deposit 0.1
